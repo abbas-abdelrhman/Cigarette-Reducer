82b34c60aadbfc8194daa96bc15c4d527442a396
abbas-abdelrhman <54155369+abbas-abdelrhman@users.noreply.github.com>
2021-03-31 08:05:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cigarette-Reducer
+ This app will help smoker to get rid of smoking
---
+++ android/app/src/main/kotlin/com/example/reduce_cigarettes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.reduce_cigarettes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
