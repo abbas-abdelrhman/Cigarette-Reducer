50d2524834af6cc2099fc179373d5b632004d0a5
abbas-abdelrhman <54155369+abbas-abdelrhman@users.noreply.github.com>
2021-03-31 17:23:14 +0200

    change challenge phases to be levels. add more pages
    change challenge phases to be levels.
    add more pages

--- lib/main.dart
+++ lib/main.dart
@@ -6,166 +6,316 @@ void main() {
   ));
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _State createState() => _State();
-}
-
-class _State extends State<MyApp> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext ctx) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Cigarette Reducer'),
-        ),
         body: Padding(
             padding: EdgeInsets.all(10),
-            child: ListView(
-              children: <Widget>[
-                Container(
-                    alignment: Alignment.center,
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      'Welcome wih us!',
-                      style: TextStyle(
-                          color: Colors.blue,
-                          fontWeight: FontWeight.w500,
-                          fontSize: 30),
-                    )),
-                Container(
-                    height: 50,
-                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                    child: RaisedButton(
-                      textColor: Colors.white,
-                      color: Colors.blue,
-                      child: Text('Phase One'),
-                      onPressed: () {
-                        print(nameController.text);
-                        print(passwordController.text);
-                      },
-                    )),
-                Container(
-                    height: 50,
-                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                    child: RaisedButton(
-                      textColor: Colors.white,
-                      color: Colors.blue,
-                      child: Text('Phase Two'),
-                      onPressed: () {
-                        print(nameController.text);
-                        print(passwordController.text);
-                      },
-                    )),
-
-                Container(
-                    height: 50,
-                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                    child: RaisedButton(
-                      textColor: Colors.white,
-                      color: Colors.blue,
-                      child: Text('Phase Three'),
-                      onPressed: () {
-                        print(nameController.text);
-                        print(passwordController.text);
-                      },
-                    )),
-                Container(
-                    height: 50,
-                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                    child: RaisedButton(
-                      textColor: Colors.white,
-                      color: Colors.blue,
-                      child: Text('Phase Four'),
-                      onPressed: () {
-                        print(nameController.text);
-                        print(passwordController.text);
-                      },
-                    )),
-
-                Container(
-                    height: 50,
-                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-                    child: RaisedButton(
-                      textColor: Colors.white,
-                      color: Colors.blue,
-                      child: Text('Challenge'),
-                      onPressed: () {
-                        print(nameController.text);
-                        print(passwordController.text);
-                      },
-                    )),
-
-
-
-
-                Container(
-                    alignment: Alignment.center,
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      'Sign in',
-                      style: TextStyle(fontSize: 20),
-                    )),
-                Container(
+            child: ListView(children: <Widget>[
+              Container(
+                  alignment: Alignment.center,
                   padding: EdgeInsets.all(10),
-                  child: TextField(
-                    controller: nameController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'User Name',
-                    ),
+                  child: Text(
+                    'Welcome wih Cigarette Reducer',
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.w500,
+                        fontSize: 30),
+                  )),
+              Container(
+                  alignment: Alignment.center,
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    'Sign in',
+                    style: TextStyle(fontSize: 20),
+                  )),
+              Container(
+                padding: EdgeInsets.all(10),
+                child: TextField(
+                  controller: TextEditingController(),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'User Name',
                   ),
                 ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                  child: TextField(
-                    obscureText: true,
-                    controller: passwordController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                    ),
+              ),
+              Container(
+                padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                child: TextField(
+                  obscureText: true,
+                  controller: TextEditingController(),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
                   ),
                 ),
-                FlatButton(
-                  onPressed: () {
-                    //forgot password screen
-                  },
-                  textColor: Colors.blue,
-                  child: Text('Forgot Password'),
-                ),
-                Container(
-                    height: 50,
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-                    child: RaisedButton(
-                      textColor: Colors.white,
-                      color: Colors.blue,
-                      child: Text('Login'),
-                      onPressed: () {
-                        print(nameController.text);
-                        print(passwordController.text);
-                      },
-                    )),
-                Container(
-                    child: Row(
-                  children: <Widget>[
-                    Text('Does not have account?'),
-                    FlatButton(
-                      textColor: Colors.blue,
-                      child: Text(
-                        'Sign in',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      onPressed: () {
-                        //signup screen
-                      },
-                    )
-                  ],
-                  mainAxisAlignment: MainAxisAlignment.center,
-                ))
-              ],
-            )));
+              ),
+              FlatButton(
+                onPressed: () {
+                  //forgot password screen
+                },
+                textColor: Colors.blue,
+                child: Text('Forgot Password'),
+              ),
+              Container(
+                  height: 50,
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                  child: RaisedButton(
+                    textColor: Colors.white,
+                    color: Colors.blue,
+                    child: Text('Login'),
+                    onPressed: () {
+                      Navigator.push(ctx, PageTwo());
+                    },
+                  )),
+              Container(
+                  child: Row(
+                children: <Widget>[
+                  Text('Does not have account?'),
+                  FlatButton(
+                    textColor: Colors.blue,
+                    child: Text(
+                      'Sign up',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    onPressed: () {
+                      //signup screen
+                    },
+                  )
+                ],
+                mainAxisAlignment: MainAxisAlignment.center,
+              ))
+            ])));
   }
+}
+
+class PageTwo extends MaterialPageRoute<Null> {
+  PageTwo()
+      : super(builder: (BuildContext ctx) {
+          return Scaffold(
+              appBar: AppBar(
+                title: Text('Challenge Phases'),
+                backgroundColor: Theme.of(ctx).canvasColor,
+                elevation: 1.0,
+              ),
+              body: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: ListView(children: <Widget>[
+                    Container(
+                        alignment: Alignment.center,
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          'Challenge Levels',
+                          style: TextStyle(
+                              color: Colors.blue,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 30),
+                        )),
+                    Container(
+                        height: 50,
+                        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                        child: RaisedButton(
+                          textColor: Colors.white,
+                          color: Colors.blue,
+                          child: Text('Fighter '),
+                          onPressed: () {
+                            Navigator.push(ctx, PageFighter());
+                          },
+                        )),
+                    Container(
+                        height: 50,
+                        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                        child: RaisedButton(
+                          textColor: Colors.white,
+                          color: Colors.blue,
+                          child: Text('Silver Fighter'),
+                          onPressed: () {
+                            Navigator.push(ctx, PageSFighter());
+                          },
+                        )),
+                    Container(
+                        height: 50,
+                        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                        child: RaisedButton(
+                          textColor: Colors.white,
+                          color: Colors.blue,
+                          child: Text('Gold Fighter'),
+                          onPressed: () {
+                            Navigator.push(ctx, PageGFighter());
+                          },
+                        )),
+                    Container(
+                        height: 50,
+                        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+                        child: RaisedButton(
+                          textColor: Colors.white,
+                          color: Colors.blue,
+                          child: Text('Diamond Fighter'),
+                          onPressed: () {
+                            Navigator.push(ctx, PageDFighter());
+                          },
+                        )),
+                    Container(
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(10),
+                      child: RaisedButton(
+                        onPressed: () {
+                          Navigator.push(ctx, Dashboard());
+                        },
+                        child: Text("Go to Dashboard"),
+                      ),
+                    ),
+                  ])));
+        });
+}
+
+class Dashboard extends MaterialPageRoute<Null> {
+  Dashboard()
+      : super(builder: (BuildContext ctx) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Back to challenge phases"),
+              backgroundColor: Theme.of(ctx).accentColor,
+              elevation: 2.0,
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () {
+                    Navigator.pop(ctx);
+                  },
+                )
+              ],
+            ),
+            body: Center(
+              child: Text(
+                'Dashboard',
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 30),
+              ),
+            ),
+          );
+        });
+}
+
+class PageFighter extends MaterialPageRoute<Null> {
+  PageFighter()
+      : super(builder: (BuildContext ctx) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Back to challenge phases"),
+              backgroundColor: Theme.of(ctx).accentColor,
+              elevation: 2.0,
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () {
+                    Navigator.pop(ctx);
+                  },
+                )
+              ],
+            ),
+            body: Center(
+              child: Text(
+                'You are in Fighter Level',
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 30),
+              ),
+            ),
+          );
+        });
+}
+
+class PageSFighter extends MaterialPageRoute<Null> {
+  PageSFighter()
+      : super(builder: (BuildContext ctx) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Back to challenge phases"),
+              backgroundColor: Theme.of(ctx).accentColor,
+              elevation: 2.0,
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () {
+                    Navigator.pop(ctx);
+                  },
+                )
+              ],
+            ),
+            body: Center(
+              child: Text(
+                ' You are in Silver Fighter Level',
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 30),
+              ),
+            ),
+          );
+        });
+}
+
+class PageGFighter extends MaterialPageRoute<Null> {
+  PageGFighter()
+      : super(builder: (BuildContext ctx) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Back to challenge phases"),
+              backgroundColor: Theme.of(ctx).accentColor,
+              elevation: 2.0,
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () {
+                    Navigator.pop(ctx);
+                  },
+                )
+              ],
+            ),
+            body: Center(
+              child: Text(
+                ' You are in Gold Fighter Level',
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 30),
+              ),
+            ),
+          );
+        });
+}
+
+class PageDFighter extends MaterialPageRoute<Null> {
+  PageDFighter()
+      : super(builder: (BuildContext ctx) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text("Back to challenge phases"),
+              backgroundColor: Theme.of(ctx).accentColor,
+              elevation: 2.0,
+              actions: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () {
+                    Navigator.pop(ctx);
+                  },
+                )
+              ],
+            ),
+            body: Center(
+              child: Text(
+                ' You are in Diamond Fighter Level',
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 30),
+              ),
+            ),
+          );
+        });
 }
